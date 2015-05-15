eb0c5c1715614cfa7758acb4ededa7b09a1a38f2
Hadrien Milano <hadrien.milano@gmail.com>
2015-05-15 12:38:06 +0200

    change config structure

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('local.courseadvisor.com')
+	//'local' => array('local.courseadvisor.com')
 
 ));
 
