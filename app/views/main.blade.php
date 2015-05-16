799ea256a08ea7ab97cd35f724f6c14a20ef2e1a
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2015-05-16 17:38:34 +0200

    fix logout redirect

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -65,7 +65,7 @@
                 <li><a href="{{{ action('AdminController@index') }}}"<i class="fa fa-lock"></i> Admin</a></li>
                 @endif
                 <li><a href="{{{ action('StudentController@dashboard') }}}"><i class="fa fa-dashboard"></i> {{{ trans('global.dashboard-action') }}}</a></li>
-                <li><a href="{{{ action('AuthController@logout') }}}"><i class="fa fa-sign-out"></i> {{{ trans('global.logout-action') }}}</a></li>
+                <li><a href="{{{ action('AuthController@logout', ['next' => Request::root()]) }}}"><i class="fa fa-sign-out"></i> {{{ trans('global.logout-action') }}}</a></li>
               </ul>
             </li>
             @else
