b7ffeb6d1bf40c4636141efdd1dcd65ef7223156
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2015-05-30 11:56:34 +0200

    Added google analytics

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -158,6 +158,16 @@
     {{ HTML::script("js/vendor/bootstrap.min.js") }}
     {{ HTML::script("js/starbar.js") }}
 
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-63571870-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
     <script>
       // Initialize starbars
       $('[data-starbar]').each(function(el){
