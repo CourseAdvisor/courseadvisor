7ca436142a3a6ae683d721ec9af18826189d474a
Hadrien Milano <hadrien.milano@gmail.com>
2015-06-04 17:22:09 +0200

    fix footer

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <div class="row">
-      <div class="col-lg-7 col-sm-4 col-xs-12">
+      <div class="col-xs-12">
         <ul>
           @if(!Tequila::isLoggedIn())
             <li><a href="{{{ action('CourseController@studyCycles') }}}">{{{ trans('global.browse-courses-action') }}}</a></li>
@@ -11,12 +11,8 @@
             <li><a href="{{{ action('AuthController@logout', ['next' => Request::root()]) }}}">{{{ trans('global.logout-action') }}}</a></li>
           @endif
           <li><a href="{{ action('StaticController@about') }}">{{{ trans('global.about-action') }}}</a></li>
+          <li class="pull-right"><iframe src="https://ghbtns.com/github-btn.html?user=courseadvisor&repo=courseadvisor&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe></li>
         </ul>
-      </div>
-      <div class="col-lg-5 col-md-5 col-sm-8 col-xs-10 copyright">
-        <div class="pull-right">
-          <iframe src="https://ghbtns.com/github-btn.html?user=courseadvisor&repo=courseadvisor&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe>
-        </div>
       </div>
     </div>
   </div>
