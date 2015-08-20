c1893e84c16f93ac8c6cca425e8d086d7021c9d4
Hadrien Milano <hadrien.milano@gmail.com>
2015-08-21 01:24:59 +0200

    trying travis environement detection

---
+++ app/config/ci/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+  'default' => 'mysql',
+
+  'connections' => array(
+    'mysql' => array(
+      'driver'    => 'mysql',
+      'host'      => 'localhost',
+      'database'  => 'courseadvisor',
+      'username'  => 'root',
+      'password'  => '',
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
+    ),
+  )
+);
