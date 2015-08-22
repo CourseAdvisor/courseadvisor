13206e9d3f4434eec4554d689303ee40ba070ba6
Hadrien Milano <hadrien.milano@gmail.com>
2015-08-22 17:18:10 +0200

    cleaning testing config

--- app/config/testing/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'debug' => false,
-    'mixpanel_key' => '411b1666a88c235a0f7213835f9b468f'
-);
--- app/config/testing/tequila.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return array(
-  'server_url'    => 'http://localhost:3000/',
-);
