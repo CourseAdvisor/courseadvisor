f0e548271d5b17fdae546240415c2bf62f1b6e9e
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2016-12-18 21:21:56 +0200

    Spelling enhancements

--- app/lang/en/censorship.php
+++ app/lang/en/censorship.php
@@ -2,8 +2,8 @@
 
 return array(
   'create-review-header' => 'Sorry!',
-  'create-review-body' => 'Due to a complaint from the school administration, it is no longer possible to express your opinion on CourseAdvisor.<br />'.
-  'If you want to learn more about the issue or participate in the debate, head over to our :facebook page.',
-  'global-banner' => 'Operations on CourseAdvisor have stopped for an unspecified duration due to a complaint from EPFL.'.
-  'It is no longer possible to write new reviews and the course listing will not be updated. You can nonetheless still log-in to edit and/or remove published reviews.'.
+  'create-review-body' => 'Due to a complaint from EPFL administration, it is no longer possible to post reviews on CourseAdvisor.<br />'.
+  'If you want to learn more about the issue or participate to the debate, head over our :facebook page.',
+  'global-banner' => 'Operations on CourseAdvisor have stopped for an unknown amount of time due to a complaint from EPFL.'.
+  'It is no longer possible to write new reviews and the course listing will not be updated. You can nonetheless still log in to edit or remove the reviews you have published.'.
   '<br />To learn more, visit our :facebook page.');
--- app/lang/fr/censorship.php
+++ app/lang/fr/censorship.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-  'create-review-header' => 'Désolé!',
-  'create-review-body' => 'En raison d\'une plainte de la part de l\'administration de l\'EPFL, il n\'est actuellement plus possible d\'exprimer ton avis sur CourseAdvisor.<br />'.
-  'Pour en savoir plus et participer au débat, visite notre page :facebook.',
-  'global-banner' => 'L\'activité sur le site de CourseAdvisor est arrêtée jusqu\'à nouvel ordre en raison d\'une plainte de la part de l\'administration de l\'EPFL. '.
-  'L\'ajout de nouveaux avis a été bloqué et la liste des cours ne sera pas mise à jour. Il vous est toutefois toujours possible de vous connecter à votre compte '.
-  'afin de modifier et/ou supprimer vos avis déjà publiés.<br />Pour en savoir plus, rendez-vous sur notre page :facebook.');
+  'create-review-header' => 'Désolé !',
+  'create-review-body' => 'En raison d\'une plainte de l\'administration de l\'EPFL, il n\'est actuellement plus possible de poster des avis sur CourseAdvisor.<br />'.
+  'Pour en savoir plus et participer au débat, rendez-vous sur notre page :facebook.',
+  'global-banner' => 'L\'activité sur CourseAdvisor est suspendue jusqu\'à nouvel ordre en raison d\'une plainte de l\'administration de l\'EPFL. '.
+  'L\'ajout de nouveaux avis a été bloqué et la liste des cours ne sera pas mise à jour. Il t\'est toutefois toujours possible de te connecter à ton compte '.
+  'afin de modifier ou supprimer tes avis déjà publiés.<br />Pour en savoir plus, rendez-vous sur notre page :facebook.');
